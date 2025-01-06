eee76bc1addec71d315d567a6e74bc491f18119f
thenameisajay <amahadeven@economicdatasciences.com>
2025-01-06 12:12:27 +0000

    essentials to standardise codebase

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,121 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import tanstackQuery from '@tanstack/eslint-plugin-query';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  {
+    ignores: [
+      '**/tailwind.config.js',
+      '**/postcss.config.js',
+      '**/prettier.config.mjs',
+      '**/prettier.config.js',
+      '**/webpack.config.cjs',
+      'public/compiled',
+      '**/next.config.mjs',
+      '**/target',
+    ],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:@tanstack/eslint-plugin-query/recommended',
+      'next/core-web-vitals',
+    ),
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+      react: fixupPluginRules(react),
+      'react-hooks': fixupPluginRules(reactHooks),
+    },
 
-export default eslintConfig;
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2021,
+      sourceType: 'module',
+
+      parserOptions: {
+        project: true,
+      },
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/await-thenable': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/prefer-optional-chain': 'warn',
+      '@typescript-eslint/no-floating-promises': 'warn',
+
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+
+      '@typescript-eslint/no-unused-vars': 'warn',
+
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+
+      'react/button-has-type': [
+        'error',
+        {
+          button: true,
+          submit: true,
+          reset: true,
+        },
+      ],
+
+      'react/hook-use-state': [
+        'off',
+        {
+          allowDestructuredState: true,
+        },
+      ],
+
+      'react/prop-types': 'off',
+    },
+  },
+];
